8663f8786c3be504d2e380e0f78d9126c6b12da3
정수진 <dmg06017@knu.ac.kr>
2023-01-10 19:38:30 +0900

    Add Room Get query

--- app/src/main/java/com/gdsc/fourcutalbum/viewmodel/FourCutsViewModel.kt
+++ app/src/main/java/com/gdsc/fourcutalbum/viewmodel/FourCutsViewModel.kt
@@ -6,6 +6,9 @@ import com.gdsc.fourcutalbum.data.model.FourCuts
 import com.gdsc.fourcutalbum.data.repository.FourCutsRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class FourCutsViewModel(
@@ -21,5 +24,8 @@ class FourCutsViewModel(
         fourCutsRepository.deleteFourCuts(fourCuts)
     }
 
-    val getFourCuts: Flow<List<FourCuts>> = fourCutsRepository.getFourCuts()
+    // val getFourCuts: Flow<List<FourCuts>> = fourCutsRepository.getFourCuts()
+    // TestActivity의 lifecycle과 동기화
+    val getFourCuts: StateFlow<List<FourCuts>> = fourCutsRepository.getFourCuts()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), listOf())
 }
