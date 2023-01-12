297213db68eab7ad859b5e2aa8346250fad7644b
dohun_ <35682247+parkdohuni@users.noreply.github.com>
2023-01-12 11:12:38 +0900

    Feat: print friends list

--- app/src/main/java/com/gdsc/fourcutalbum/DetailActivity.kt
+++ app/src/main/java/com/gdsc/fourcutalbum/DetailActivity.kt
@@ -7,20 +7,18 @@ import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
+import com.gdsc.fourcutalbum.adapter.DetailAdapter
 import com.gdsc.fourcutalbum.data.db.FourCutsDatabase
 import com.gdsc.fourcutalbum.data.model.FourCuts
 import com.gdsc.fourcutalbum.data.repository.FourCutsRepositoryImpl
 import com.gdsc.fourcutalbum.databinding.ActivityDetailBinding
-import com.gdsc.fourcutalbum.viewmodel.FourCutsViewModel
-import com.gdsc.fourcutalbum.viewmodel.MainViewModel
-import com.gdsc.fourcutalbum.viewmodel.MainViewModelProviderFactory
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.stateIn
@@ -29,8 +27,9 @@ import kotlinx.coroutines.launch
 class DetailActivity: AppCompatActivity() {
     lateinit var imageUri: Uri
     lateinit var binding: ActivityDetailBinding
-    lateinit var fourCutsViewModel: FourCutsViewModel
-
+    lateinit var recyclerView: RecyclerView
+    lateinit var viewAdapter: DetailAdapter
+    lateinit var viewManager: RecyclerView.LayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -43,8 +42,9 @@ class DetailActivity: AppCompatActivity() {
         val id = intent.getIntExtra("id",0)
         val database = FourCutsDatabase.getInstance(this)
         val fourCutsRepository = FourCutsRepositoryImpl(database)
-        val factory = MainViewModelProviderFactory(fourCutsRepository)
-        val fourCuts = fourCutsRepository.getFourCutsWithId(id).stateIn(lifecycleScope, SharingStarted.WhileSubscribed(5000), FourCuts("", Uri.EMPTY, listOf(),"",""))
+        val fourCuts = fourCutsRepository.getFourCutsWithId(id).stateIn(lifecycleScope, SharingStarted.WhileSubscribed(5000),
+            FourCuts("", Uri.EMPTY, listOf(),"",""))
+        viewManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -53,9 +53,12 @@ class DetailActivity: AppCompatActivity() {
                     it.apply {
                         binding.titleTv.text = title
                         binding.locationTv.text = place
-                        binding.commentContentTv.text = comment
-                        // TODO :: friend chip 구현 필요
-
+                        binding.commentContentTv.text = comment // TODO :: friend chip 구현 필요
+                        viewAdapter = fourCuts.value.friends?.let { DetailAdapter(it) }!!
+                        recyclerView = binding.nameRv.apply {
+                            layoutManager = viewManager
+                            adapter = viewAdapter
+                        }
                         Glide.with(binding.root.context).load(it.photo)
                             .override(Target.SIZE_ORIGINAL)
                             .apply(RequestOptions().override(600, 600))
@@ -81,7 +84,6 @@ class DetailActivity: AppCompatActivity() {
         }
 
     }
-
 
     // 이미지를 결과값으로 받는 변수
     private val imageResult = registerForActivityResult(
