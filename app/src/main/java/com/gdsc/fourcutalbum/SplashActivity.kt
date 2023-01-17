db5da8d13a4abdfabe5cd77f8718a7358475b918
Glacier Han <hbh6449@naver.com>
2023-01-17 20:39:51 +0900

    Edit SplashActivity

--- app/src/main/java/com/gdsc/fourcutalbum/SplashActivity.kt
+++ app/src/main/java/com/gdsc/fourcutalbum/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.gdsc.fourcutalbum.databinding.ActivityMainBinding
 import com.gdsc.fourcutalbum.databinding.ActivitySplashBinding
@@ -17,6 +18,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        findViewById<LinearLayout>(R.id.splash).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
         setInitialize()
     }
 
@@ -24,6 +29,6 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 1800)
+        }, 3500)
     }
 }
